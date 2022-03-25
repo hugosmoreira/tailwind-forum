12dbc2018fa4e9470c1aec5bf518e8a9550f562d
hugosmoreira <hugo.n4@gmail.com>
2022-03-24 19:45:33 -0700

    set up boiler plate and start sidebar

---
+++ components/Sidebar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { FiMoreHorizontal } from 'react-icons/fi'
+import { VscTwitter } from 'react-icons/vsc'
+
+
+
+const style = {
+    wrapper: `flex-[0.7] px-8 flex flex-col`,
+    twitterIconContainer: `text-3xl m-4`,
+    twitterButton: `bg-[#1d9bf0] hover:bg-[#1b8cd8] flex items-center justify-center font-bold roundeded-3xl h-[50px] mt-[20px] `,
+    navContainer: `flex-1`,
+    profileButton: `flex items-center mb-6 cursor-pointer hover:bg-[#1b8cd8]`,
+    profileLeft: `flex items-center justify-center mr-4`,
+    profileImage: `height-12 w-12 rounded-full`,
+    profileRight: `flex-1 flex`,
+    details: `flex-1`,
+    name: `text-lg`,
+    handle: `text-[#8899a6]`,
+    moreContainer: `flex items-center mr-2`,
+}
+
+const Sidebar = () => {
+  return (
+    <div>Sidebar</div>
+  )
+}
+
+export default Sidebar
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Sidebar from '../components/sidebar'
+
+
+
+const style =  {
+  wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
+  content: `max-w-[1400px] w-2/3 flex justify-between`, 
+}
+
+const Home = () => {
+  return (
+    <div className={style.wrapper}>
+     <div className={style.content}>
+        <Sidebar />
+        <h2>Feed</h2>
+        <h2>Widgets</h2>
+     </div>
+    </div>
+  )
+}
+
+export default Home
