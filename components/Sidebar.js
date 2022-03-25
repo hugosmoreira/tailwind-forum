b2f05bf3ba460a17ab40c77e1362ac2141775b63
hugosmoreira <hugo.n4@gmail.com>
2022-03-24 20:17:35 -0700

    sidebar probably complete change icon later

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import { FiMoreHorizontal } from 'react-icons/fi'
+import { useState, useContext } from 'react'
+import { useRouter } from 'next/router'
+import SidebarOption from './SidebarOption'
+import { RiHome7Line, RiHome7Fill, RiFileList2Fill } from 'react-icons/ri'
+import { BiHash } from 'react-icons/bi'
+import { FiBell, FiMoreHorizontal } from 'react-icons/fi'
+import { HiOutlineMail, HiMail } from 'react-icons/hi'
+import { FaRegListAlt, FaHashtag, FaBell } from 'react-icons/fa'
+import { CgMoreO } from 'react-icons/cg'
 import { VscTwitter } from 'react-icons/vsc'
-
-
+import {
+  BsBookmark,
+  BsBookmarkFill,
+  BsPerson,
+  BsPersonFill,
+} from 'react-icons/bs'
 
 const style = {
     wrapper: `flex-[0.7] px-8 flex flex-col`,
     twitterIconContainer: `text-3xl m-4`,
-    twitterButton: `bg-[#1d9bf0] hover:bg-[#1b8cd8] flex items-center justify-center font-bold roundeded-3xl h-[50px] mt-[20px] `,
+    tweetButton: `bg-[#1d9bf0] hover:bg-[#1b8cd8] flex items-center justify-center font-bold rounded-3xl h-[50px] mt-[20px] cursor-pointer`,
     navContainer: `flex-1`,
     profileButton: `flex items-center mb-6 cursor-pointer hover:bg-[#1b8cd8]`,
     profileLeft: `flex items-center justify-center mr-4`,
@@ -19,10 +30,92 @@ const style = {
     moreContainer: `flex items-center mr-2`,
 }
 
-const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
+const Sidebar = ({ initialSelectedIcon = 'Home' }) => {
+
+    const [selected, setSelected] = useState(initialSelectedIcon)
+
+    return (
+        <div className={style.wrapper}>
+          <div className={style.twitterIconContainer}>
+            <VscTwitter />
+          </div>
+          <div className={style.navContainer}>
+            <SidebarOption
+              Icon={selected === 'Home' ? RiHome7Fill : RiHome7Line}
+              text='Home'
+              isActive={Boolean(selected === 'Home')}
+              setSelected={setSelected}
+              redirect={'/'}
+            />
+            <SidebarOption
+              Icon={selected === 'Explore' ? FaHashtag : BiHash}
+              text='Explore'
+              isActive={Boolean(selected === 'Explore')}
+              setSelected={setSelected}
+            />
+            <SidebarOption
+              Icon={selected === 'Notifications' ? FaBell : FiBell}
+              text='Notifications'
+              isActive={Boolean(selected === 'Notifications')}
+              setSelected={setSelected}
+            />
+            <SidebarOption
+              Icon={selected === 'Messages' ? HiMail : HiOutlineMail}
+              text='Messages'
+              isActive={Boolean(selected === 'Messages')}
+              setSelected={setSelected}
+            />
+            <SidebarOption
+              Icon={selected === 'Bookmarks' ? BsBookmarkFill : BsBookmark}
+              text='Bookmarks'
+              isActive={Boolean(selected === 'Bookmarks')}
+              setSelected={setSelected}
+            />
+            <SidebarOption
+              Icon={selected === 'Lists' ? RiFileList2Fill : FaRegListAlt}
+              text='Lists'
+              isActive={Boolean(selected === 'Lists')}
+              setSelected={setSelected}
+            />
+            <SidebarOption
+              Icon={selected === 'Profile' ? BsPersonFill : BsPerson}
+              text='Profile'
+              isActive={Boolean(selected === 'Profile')}
+              setSelected={setSelected}
+              redirect={'/profile'}
+            />
+            <SidebarOption Icon={CgMoreO} text='More' />
+            <div
+              onClick={() =>
+                router.push(`${router.pathname}/?mint=${currentAccount}`)
+              }
+              className={style.tweetButton}
+            >
+              Mint
+            </div>
+          </div>
+          <div className={style.profileButton}>
+            <div className={style.profileLeft}>
+              <img
+               
+              />
+            </div>
+            <div className={style.profileRight}>
+              <div >
+                <div ></div>
+                <div >
+                  
+                </div>
+              </div>
+              <div className={style.moreContainer}>
+                <FiMoreHorizontal />
+              </div>
+            </div>
+          </div>
+    
+          
+        </div>
+      )
 }
 
 export default Sidebar
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,16 @@ import Sidebar from '../components/sidebar'
 
 
 
+
 const style =  {
   wrapper: `flex justify-center h-screen w-screen select-none bg-[#15202b] text-white`,
   content: `max-w-[1400px] w-2/3 flex justify-between`, 
 }
 
 const Home = () => {
+
+  
+
   return (
     <div className={style.wrapper}>
      <div className={style.content}>
